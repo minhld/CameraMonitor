5f399183b28beba195c323ece8b23f4c4c47aac5
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-05-28 23:38:50 -0600

    added the odom listener

---
+++ src/OdomListener.java
@@ -0,0 +1,28 @@
+import org.ros.message.MessageListener;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Subscriber;
+
+import nav_msgs.Odometry;
+
+public class OdomListener extends AbstractNodeMain {
+
+	@Override
+	public GraphName getDefaultNodeName() {
+		return GraphName.of("wheelchair/odom");
+	}
+	
+	@Override
+	public void onStart(ConnectedNode connectedNode) {
+	    Subscriber<Odometry> subscriber = connectedNode.newSubscriber("odom", Odometry._TYPE);
+	    subscriber.addMessageListener(new MessageListener<Odometry>() {
+			@Override
+			public void onNewMessage(Odometry pos) {
+				System.out.println("current velocity: " + pos.getTwist().getTwist().getLinear().getX());
+			}
+		});
+
+	}
+
+}
