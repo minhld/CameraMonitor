30a88178d87c046231ca486e1a6b2e633f4dbb0f
minhld <ledinhminh3883@gmail.com>
2017-06-06 01:23:53 -0600

    updated test samples

--- src/com/minhld/temp/TemplateMatching.java
+++ src/com/minhld/temp/TemplateMatching.java
@@ -15,8 +15,8 @@ public class TemplateMatching {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Mat source=null;
 		Mat template=null;
-		source=Imgcodecs.imread("samples/realcam2.jpg");
-		template=Imgcodecs.imread("samples/design1.png");
+		source = Imgcodecs.imread("samples/realcam2.jpg");
+		template=Imgcodecs.imread("samples/design6.png");
 	
 		Mat outputImage=new Mat();	
 		int machMethod=Imgproc.TM_CCOEFF;
