2ffeed697ee18857e6b6969ba055c61c67865bad
minhld <ledinhminh3883@gmail.com>
2017-07-05 01:47:39 -0600

    added some minor updates

--- src/com/minhld/ros/controller/RosAutoRed.java
+++ src/com/minhld/ros/controller/RosAutoRed.java
@@ -665,12 +665,12 @@ public class RosAutoRed extends Thread {
 		
 	}
 	
-	int count = 0;
+	int count = 1;
 	double maxX = -100, minX = 100, maxY = -100, minY = 100;
 	
 	private void drawWheelchairPoint(double distance, double angle) {
 		Point wcPoint = FeatureExtractorRed.findPointByAngle(distance, angle);
-		if (count < 5) {
+		if (count % 5 != 0) {
 			count++;
 			if (maxX < wcPoint.x) {
 				maxX = wcPoint.x;
@@ -685,15 +685,19 @@ public class RosAutoRed extends Thread {
 				minY = wcPoint.y;
 			}
 		} else {
-			double x = (minX + maxX) / 2;
-			double y = (minY + maxY) / 2;
-			double rad = Math.sqrt(Math.pow(maxX - minX, 2) + Math.pow(maxY - minY, 2));
-			LocationDrawer.updateData(new Point(x, y), rad / 2);
-			count = 0;
-//			maxX = -100;
-//			minX = 100; 
-//			maxY = -100;
-//			minY = 100;
+			if (count < 50) {
+				double x = (minX + maxX) / 2;
+				double y = (minY + maxY) / 2;
+				double rad = Math.sqrt(Math.pow(maxX - minX, 2) + Math.pow(maxY - minY, 2));
+				LocationDrawer.updateData(new Point(x, y), rad / 2);
+				count++;
+			} else {
+				count = 1;
+				maxX = -100;
+				minX = 100; 
+				maxY = -100;
+				minY = 100;
+			}
 		}
 		
 	}
